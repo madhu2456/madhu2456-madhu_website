2e786fbab61bb27a093bf999653af5c79044afab
Madhu Dadi <madhu.kumar245@gmail.com>
2026-04-07 20:19:47 +0530

    fix favicon type issue

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -24,13 +24,25 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
+      },
+      {
+        src: "/icon.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
