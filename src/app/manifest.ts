eac75098466d4c22d1d64e0b24dd1bf62688f675
Madhu Dadi <madhu.kumar245@gmail.com>
2026-04-07 20:15:53 +0530

    fix favicon issues on SERP

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,8 +16,21 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/favicon.ico",
-        sizes: "any",
+        sizes: "48x48",
         type: "image/x-icon",
+        purpose: "any",
+      },
+      {
+        src: "/icon.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
     ],
   };
